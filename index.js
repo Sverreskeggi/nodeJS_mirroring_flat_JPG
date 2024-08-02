3232245f475ea1ebf48dc07876ed5811156e983b
Sverreskeggi <alexanderpoilov85@gmail.com>
2024-08-03 02:28:47 +0300

    done, but not recurse and not CLI and not universal

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-const { log } = require('console');
 const fs = require('fs');
-// const Jimp = require('jimp') ;
 
-// async function reverseImageJpg(saveFile) {
-//   const image = await Jimp.read('0ov4a.jpg');
-//   image.flip(true, false, function(err){
-//      if (err) throw err;
-//   })
-//   .write(`mirror_${saveFile}`);
-// }
+const Jimp = require('jimp') ;
 
-const recurseFolder = (path) => {
+async function reverseImageJpg(path, file) {
+  console.log('input file: ', path + file);
+  
+  const image = await Jimp.read(`${path}${file}`);
+  image.flip(true, false, function(err){
+     if (err) throw err;
+  })
+  .write(`${path}mirror_${file}`);
+}
+
+const recurseFolder = async (path) => {
   fs.readdir(path, (err, files) => {
     if (err) throw err;
     
     for (let file of files) {
-      fs.stat(path, (errStat, status) => { 
+      fs.stat(`${path}${file}`, (errStat, status) => { 
         if (errStat) throw errStat;
 
-        if (status.isDirectory()) {
-          console.log(`Folder: ${file}`);
-          recurseFolder(`${path}/${file}`);
-        } else {
-          console.log(`File: ${file}`);
-        }
+        if (status.isFile()) {
+            reverseImageJpg(path, file)
+        } 
       });
     }
   });
 }
 
-recurseFolder('oven/');
+
+recurseFolder('../oven/Печка вид/');
+recurseFolder('../oven/Печка дополнительно/');
+recurseFolder('../oven/Печка изменения для работы камина/');
+recurseFolder('../oven/Печка порядовка/');
